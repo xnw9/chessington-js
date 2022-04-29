b55ed3a25d09c5a597ff33ac211799f3d1e48292
Sydney Wu <xinni.wu@softwire.com>
2022-04-29 14:41:33 +0100

    Pass Red-9 & improve pawn

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -14,8 +14,12 @@ export default class Rook extends Piece {
         for (let row = 0; row < 8; row++) {
             let col = current.col
             let square = Square.at(row, col)
-            if (!board.checkOccupancy(square) && square.row != current.row) {   // not to include current position
-                available.push(square)
+
+            if (square.row != current.row) {        // not including itself
+                if (board.checkOccupancy(square)) {         // stop as soon as meeting another piece
+                    break
+                }
+                available.push(square)          // otherwise valid move
             }
         }
 
@@ -23,9 +27,14 @@ export default class Rook extends Piece {
         for (let col = 0; col < 8; col++) {
             let row = current.row
             let square = Square.at(row, col)
-            if (!board.checkOccupancy(square) && square.col != current.col) {
+
+            if (square.col != current.col) {
+                if (board.checkOccupancy(square)) {
+                    break
+                }
                 available.push(square)
             }
+
         }
 
         return available
